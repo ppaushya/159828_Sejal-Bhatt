431034711f9c93734639ebed76235e559f6211f1
ppaushya <43550473+ppaushya@users.noreply.github.com>
2018-09-29 11:17:31 +0530

    Add files via upload

---
+++ PackageImport.java
@@ -0,0 +1,12 @@
+package Assignments;
+import Balance.*;
+
+public class PackageImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account acc=new Account();
+		acc.display_Balance(10000);
+	}
+
+}
